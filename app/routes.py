20af801f4d3a841ac463eefc20cabd0783f7f77c
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-08 11:18:03 +0200

    Add config file

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    messages = [
+        {'type': 'sent',
+         'content': 'Salut Yoda ! Comment tu vas ?'},
+        {'type': 'incoming',
+         'content': 'Quand 900 ans comme tu auras, moins en forme tu seras'}
+    ]
+    return render_template('index.html', messages=messages)
---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    LOGO = 'https://img.icons8.com/color/150/000000/baby-yoda.png'
+    CATCHPHRASE = 'Sur YodaPy le bienvenue tu es !'
+    AUTHOR = 'Guillaume OJARDIAS'
+    SOCIAL_LINKS = [
+        {'name': 'Linkedin',
+         'url': 'https://www.linkedin.com/in/guillaume-ojardias-91a46643/'},
+        {'name': 'Github',
+         'url': 'https://github.com/GuillaumeOj'}
+    ]
+    PROFILE_LINK = next(link['url'] for link in SOCIAL_LINKS
+                        if link['name'] == 'Linkedin')
