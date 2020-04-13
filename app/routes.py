591c8dc494df0ea9f3932a513742df2c162e7c42
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-13 23:55:49 +0200

    Update for frontend

--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,17 @@ from app import app
 from app.forms import MessageFieldsForm
 
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
     messages = [
-        {"type": "sent", "content": "Salut Yoda ! Comment tu vas ?"},
+        {'type': 'sent', 'content': 'Salut Yoda ! Comment tu vas ?'},
         {
-            "type": "incoming",
-            "content": "Quand 900 ans comme tu auras, moins en forme tu seras",
+            'type': 'incoming',
+            'content': 'Quand 900 ans comme tu auras, moins en forme tu seras',
         },
+        {'type': 'sent', 'content': 'Vieux shnok !'},
     ]
 
     form = MessageFieldsForm()
-    return render_template("index.html", messages=messages, form=form)
+    return render_template('index.html', messages=messages, form=form)
--- config.py
+++ config.py
@@ -4,16 +4,30 @@ current_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    LOGO = 'https://img.icons8.com/color/150/000000/baby-yoda.png'
+    LOGO = 'https://img.icons8.com/color/130/000000/baby-yoda.png'
+    BOT_NAME = 'YodaPy'
     CATCHPHRASE = 'Sur YodaPy le bienvenue tu es !'
     AUTHOR = 'Guillaume OJARDIAS'
+
+    # To find out which class to use for social links, please see the link below
+    # https://www.nerdfonts.com/cheat-sheet
     SOCIAL_LINKS = [
-        {'name': 'Linkedin',
-         'url': 'https://www.linkedin.com/in/guillaume-ojardias-91a46643/'},
-        {'name': 'Github',
-         'url': 'https://github.com/GuillaumeOj'}
+        {
+            'profile': True,
+            'url': 'https://www.linkedin.com/in/guillaume-ojardias-91a46643/',
+            'class': 'nf-fa-linkedin_square',
+        },
+        {
+            'profile': False,
+            'url': 'https://github.com/GuillaumeOj',
+            'class': 'nf-fa-github',
+        },
+        {
+            'profile': False,
+            'url': 'https://twitter.com/GuillaumeOj',
+            'class': 'nf-fa-twitter_square',
+        },
     ]
-    PROFILE_LINK = next(link['url'] for link in SOCIAL_LINKS
-                        if link['name'] == 'Linkedin')
+    PROFILE_LINK = next(link['url'] for link in SOCIAL_LINKS if link['profile'])
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'This-is-my-secret-key'
