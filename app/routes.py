ed6428a4e0eae799688c43727ed130c95a0ddb67
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-16 17:36:41 +0200

    Clean index

--- app/routes.py
+++ app/routes.py
@@ -4,22 +4,7 @@ from app.forms import MessageFieldsForm
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    messages = [
-        {'type': 'sent', 'content': 'Salut Yoda ! Comment tu vas ?'},
-        {
-            'type': 'incoming',
-            'content': 'Quand 900 ans comme tu auras, moins en forme tu seras',
-        },
-        {'type': 'sent', 'content': 'Où se trouve OpenClassrooms ?'},
-        {'type': 'incoming', 'content': 'A vos intuitions vous fier il faut.',},
-        {'type': 'sent', 'content': 'Et plus concrétement ?'},
-        {
-            'type': 'incoming',
-            'content': 'Je ne peux rien lui apprende, cet enfant n\'a aucune patience. Trop de colère en lui, comme son père, il n\'est pas prêt.',
-        },
-    ]
-
+    """Landing page"""
     form = MessageFieldsForm()
-    return render_template('index.html', messages=messages, form=form)
+    return render_template('index.html', form=form)
