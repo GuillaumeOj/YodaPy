5dcb54b79d6fd2f49f7166630cb83ffcf6c4594a
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-17 12:13:53 +0200

    Write test for the parser

---
+++ tests/test_parser.py
@@ -0,0 +1,39 @@
+from app.parser import Parser
+
+
+class TestParser:
+    def test_lower_text(self):
+        parser = Parser('Test and Retest!')
+        expected_result = 'test and retest!'
+        parser.lower_text()
+        assert parser.question == expected_result
+
+    def test_remove_accents(self):
+        parser = Parser('éùéî')
+        expected_result = 'euei'
+        parser.remove_accents()
+        assert parser.question == expected_result
+
+    def test_split_text(self):
+        parser = Parser('First sentence. And a question? Wonderfull!')
+        expected_result = ['First sentence.', 'And a question?', 'Wonderfull!']
+        parser.split_senteces()
+        assert parser.question == expected_result
+
+    def test_find_question(self):
+        parser = Parser(
+            '''Salut Yoda, comment vas-tu ? Peux-tu me dire où trouver la tour Eiffel ?
+            Passes un bonne journée !'''
+        )
+        expected_result = 'Peux-tu me dire où trouver la tour Eiffel ?'
+        parser.find_question()
+        assert parser.question == expected_result
+
+    def test_find_location(self):
+        parser = Parser(
+            '''Salut Yoda, comment vas-tu ? Peux-tu me dire où trouver la tour Eiffel ?
+            Passes un bonne journée !'''
+        )
+        expected_result = 'tour Eiffel'
+        parser.find_location()
+        assert parser.question == expected_result
