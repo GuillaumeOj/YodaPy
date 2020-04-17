a49bca3c5ccf4d9a63e72b7702ae6fee26ddb000
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-17 17:37:08 +0200

    Change attribute name in init and add 2 methods (remove_accents and split_sentences)

--- app/parser.py
+++ app/parser.py
@@ -1,7 +1,24 @@
+import unicodedata
+
+
 class Parser:
-    def __init__(self, user_input):
-        self.user_input = user_input
-        self.question = ''
+    def __init__(self, question):
+        self.question = question
 
     def lower_text(self):
-        self.question = self.user_input.lower()
+        self.question = self.question.lower()
+
+    def remove_accents(self):
+        unicode_text = unicodedata.normalize('NFD', self.question)
+        ascii_unaccented = unicode_text.encode('ascii', 'ignore')
+        self.question = ascii_unaccented.decode('utf8')
+
+    def split_sentences(self):
+        sentences = []
+        start = 0
+        for i, character in enumerate(self.question):
+            if character in '?.!':
+                sentences.append(self.question[start:i].strip())
+                start = i + 1
+
+        self.question = sentences
