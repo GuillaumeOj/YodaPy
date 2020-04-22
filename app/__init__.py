e6affe72e17625bccc1623c9a6b3a3c7b589bfec
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-22 20:14:41 +0200

    Add token and url for the geocode api and use Black

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,6 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+app.logger.info(app.config["SECRET_KEY"])
+
 from app import routes, errors
--- config.py
+++ config.py
@@ -4,30 +4,35 @@ current_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    LOGO = 'https://img.icons8.com/color/130/000000/baby-yoda.png'
-    BOT_NAME = 'YodaPy'
-    CATCHPHRASE = 'Sur YodaPy le bienvenue tu es !'
-    AUTHOR = 'Guillaume OJARDIAS'
+    LOGO = "https://img.icons8.com/color/130/000000/baby-yoda.png"
+    BOT_NAME = "YodaPy"
+    CATCHPHRASE = "Sur YodaPy le bienvenue tu es !"
+    AUTHOR = "Guillaume OJARDIAS"
 
     # To find out which class to use for social links, please see the link below
     # https://www.nerdfonts.com/cheat-sheet
     SOCIAL_LINKS = [
         {
-            'profile': True,
-            'url': 'https://www.linkedin.com/in/guillaume-ojardias-91a46643/',
-            'class': 'nf-fa-linkedin_square',
+            "profile": True,
+            "url": "https://www.linkedin.com/in/guillaume-ojardias-91a46643/",
+            "class": "nf-fa-linkedin_square",
         },
         {
-            'profile': False,
-            'url': 'https://github.com/GuillaumeOj',
-            'class': 'nf-fa-github',
+            "profile": False,
+            "url": "https://github.com/GuillaumeOj",
+            "class": "nf-fa-github",
         },
         {
-            'profile': False,
-            'url': 'https://twitter.com/GuillaumeOj',
-            'class': 'nf-fa-twitter_square',
+            "profile": False,
+            "url": "https://twitter.com/GuillaumeOj",
+            "class": "nf-fa-twitter_square",
         },
     ]
-    PROFILE_LINK = next(link['url'] for link in SOCIAL_LINKS if link['profile'])
+    PROFILE_LINK = next(link["url"] for link in SOCIAL_LINKS if link["profile"])
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'This-is-my-secret-key'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "This-is-my-secret-key"
+
+    # Information about the geo_code API
+    # This project use Mapbox https://www.mapbox.com/
+    GEO_TOKEN = os.environ.get("GEO_TOKEN") or "This-is-a-foo-tooken"
+    GEO_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
