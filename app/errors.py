8995bb2a2fa93cd3b17b3e46d7c5a0ed85e4ba49
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-22 12:45:21 +0200

    Use Black

--- app/errors.py
+++ app/errors.py
@@ -4,14 +4,14 @@ from app import app
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('errors/404.html')
+    return render_template("errors/404.html")
 
 
 @app.errorhandler(405)
 def wrong_method_error(error):
-    return render_template('errors/405.html')
+    return render_template("errors/405.html")
 
 
 @app.errorhandler(500)
 def server_error(error):
-    return render_template('errors/500.html')
+    return render_template("errors/500.html")
