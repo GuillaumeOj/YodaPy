361470d330d22f1cbd3c0bd727dee09c24463232
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-22 22:13:25 +0200

    Add GeoCode for requesting data

---
+++ app/geo_code.py
@@ -0,0 +1,37 @@
+import requests
+from app import app
+
+
+class GeoCode:
+    def __init__(self):
+        self.geo_token = app.config["GEO_TOKEN"]
+        self.geo_url = app.config["GEO_URL"]
+        self.query_text = ""
+
+    def api_request(self, query_text):
+        self.query_text = query_text
+        url = f"{self.geo_url}/{self.query_text}.json"
+        parameters = {
+            "access_token": self.geo_token,
+        }
+
+        response = requests.get(url, params=parameters)
+
+        features = []
+
+        # Return a list of features if response is ok
+        if response.ok:
+            features = response.json()["features"]
+
+            return [
+                {
+                    "relevance": feature["relevance"],
+                    "text": feature["text"],
+                    "place_name": feature["place_name"],
+                    "center": feature["center"],
+                }
+                for feature in features
+            ]
+
+        # Return the http status code else
+        return response.status_code
---
+++ tests/test_api.py
@@ -0,0 +1,46 @@
+from app.geo_code import GeoCode
+
+
+class TestGeoCode:
+    def test_api_request(self, monkeypatch):
+        locations = [
+            {
+                "relevance": 1,
+                "text": "Tour Eiffel",
+                "place_name": "Tour Eiffel, 5 avenue Anatole France, Paris, 75007, France",
+                "center": [2.29480075, 48.85878700000001],
+            },
+            {
+                "relevance": 0.899333,
+                "text": "Parking Tour Eiffel",
+                "place_name": "Parking Tour Eiffel, 75007 Paris, France",
+                "center": [2.296247, 48.854961],
+            },
+            {
+                "relevance": 0.496667,
+                "text": "France",
+                "place_name": "France",
+                "center": [2, 47],
+            },
+        ]
+
+        class MockRequestGet:
+            def __init__(self, url, params=None):
+                pass
+                self.ok = True
+
+            def json(self):
+                return {
+                    "features": [
+                        {
+                            "relevance": location["relevance"],
+                            "text": location["text"],
+                            "place_name": location["place_name"],
+                            "center": location["center"],
+                        }
+                        for location in locations
+                    ]
+                }
+
+        monkeypatch.setattr("requests.get", MockRequestGet)
+        assert GeoCode().api_request("query_text") == locations
