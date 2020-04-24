2afea9e05438feab1bea2a11eb85f57a4c26f66e
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-24 14:35:26 +0200

    Set the map in french

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -32,12 +32,17 @@ function createMap(content) {
     // Create the map
     let map = new mapboxgl.Map({
         container: mapDiv.id, // Container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
+        style: 'mapbox://styles/mapbox/streets-v10', // Map style to use
         center: content['center'],// Starting position [lng, lat]
         zoom: 14, // Starting zoom level
     });
+    // Change map language for french
+    let language = new MapboxLanguage({
+        defaultLanguage: 'fr'
+    });
+    map.addControl(language);
     // Add a marker to the map
-    let marker = new mapboxgl.Marker() // initialize a new marker
+    new mapboxgl.Marker() // initialize a new marker
       .setLngLat(content['center']) // Marker [lng, lat] coordinates
       .addTo(map); // Add the marker to the map
 }
