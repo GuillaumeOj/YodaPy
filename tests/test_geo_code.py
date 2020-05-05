8cea44d81e090ef44331c6854eb36ab00e138fde
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-05-05 09:13:47 +0200

    Update test for geo_code

--- tests/test_geo_code.py
+++ tests/test_geo_code.py
@@ -6,23 +6,31 @@ class TestGeoCode:
         locations = [
             {
                 "relevance": 1,
-                "text": "Tour Eiffel",
-                "place_name": "Tour Eiffel, 5 avenue Anatole France, Paris, 75007, France",
+                "text_fr": "Tour Eiffel",
+                "place_name_fr": "Tour Eiffel, 5 avenue Anatole France, Paris, 75007, France",
                 "center": [2.29480075, 48.85878700000001],
             },
             {
                 "relevance": 0.899333,
-                "text": "Parking Tour Eiffel",
-                "place_name": "Parking Tour Eiffel, 75007 Paris, France",
+                "text_fr": "Parking Tour Eiffel",
+                "place_name_fr": "Parking Tour Eiffel, 75007 Paris, France",
                 "center": [2.296247, 48.854961],
             },
             {
                 "relevance": 0.496667,
-                "text": "France",
-                "place_name": "France",
+                "text_fr": "France",
+                "place_name_fr": "France",
                 "center": [2, 47],
             },
         ]
+
+        excepted_result = {
+            "relevance": locations[0]["relevance"],
+            "text_fr": locations[0]["text_fr"],
+            "place_name_fr": locations[0]["place_name_fr"],
+            "longitude": locations[0]["center"][0],
+            "latitude": locations[0]["center"][1],
+        }
 
         class MockRequestGet:
             def __init__(self, url, params=None):
@@ -35,8 +43,8 @@ class TestGeoCode:
                     "features": [
                         {
                             "relevance": location["relevance"],
-                            "text": location["text"],
-                            "place_name": location["place_name"],
+                            "text_fr": location["text_fr"],
+                            "place_name_fr": location["place_name_fr"],
                             "center": location["center"],
                         }
                         for location in locations
@@ -45,4 +53,4 @@ class TestGeoCode:
 
         monkeypatch.setattr("requests.get", MockRequestGet)
         result = GeoCode().api_request("query_text")
-        assert result == locations[0]
+        assert result == excepted_result
